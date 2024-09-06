16ae598ab149e9ea229dd07dd5e09503a326e588
Mohitgarg1111 <itzzzadii57@gmail.com>
2024-09-06 23:32:46 +0530

    first commit

---
+++ Desktop/java/first.java
@@ -0,0 +1,18 @@
+// public class first{
+// public static void main(String[] args) {
+   
+//     //   bitwise operators
+//             int p=10;
+//             int q=9;
+            
+    
+//             System.out.println(p|q);
+//             System.out.println(p&q);
+//             System.out.println(p^q);
+//             System.out.println(~q);
+//             System.out.println(p<<1);
+//             System.out.println(q>>1);
+//         }
+    
+// }
+
